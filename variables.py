d38bcea05e20918005844ff8772a3ef3d55424cf
aathan23a <77298008+aathan23a@users.noreply.github.com>
2021-01-18 22:22:52 -0500

    Create variables.py

---
+++ variables.py
@@ -0,0 +1,27 @@
+# Q1
+# Assign a message to a variable, print that message. Then change the value 
+# of the variable to a new message, and print the new message.
+var = "Hello, I am Aathan"
+print(var)
+var = "This is my program"
+print(var)
+
+# Q2
+# Use a variable to represent a person's name, and then print that person's 
+# name in lowercase, upper case, and title case.
+name = "Aathan"
+print(name.lower())
+print(name.upper())
+print(name.title())
+
+# Q3
+# Find a quote from a famous person you admire. Print the quote and name of 
+# its author. Your output include quotation marks.
+print('Micheal Jordan said “I can accept failure, everyone fails at something. But I can’t accept not trying.”')
+# Q4
+# For the above, represent the famous person in a variable named famous_person. 
+# Then compose your message and represent it with a new variable called message. 
+# Print your message.
+famous_person = 'Micheal Jordan'
+message = '“I can accept failure, everyone fails at something. But I can’t accept not trying.”'
+print (famous_person + ' said ' + message)
