aaa02062819340a18657e65f8b95089bc0bc31d9
aathan23a <77298008+aathan23a@users.noreply.github.com>
2021-01-26 17:23:31 -0500

    Assignment 2

---
+++ a_lists.py
@@ -0,0 +1,28 @@
+# Q1
+# Names. Store the names of a few of your friends in a list called names. Print 
+# each person's name by accessing each element in the list, one at a time.
+Names=['Nick' ,'John' ,'Jack' ,'Jake']
+print(Names)
+print(Names[0])
+print(Names[1])
+print(Names[2])
+print(Names[3])
+
+# Q2
+# Greetings. Start with the list from Q1, but instead of just printing the names,
+# print a message to them. The text of each message should be the same, but each 
+# message should be personalized with the person's name.
+print("Hey "+ Names[0]+"! How are you?")
+print("Hey "+ Names[1]+"! How are you?")
+print("Hey "+ Names[2]+"! How are you?")
+print("Hey "+ Names[3]+"! How are you?")
+
+# Q3
+# Own List. Think of your favourite mode of transportation, such as a motorcycle
+# or a car, and make a list that stores several examples. Use your list to print a
+# series of statements about these items, such as "I would like to own a Honda 
+# motorcycle"
+Transportation=['Airplane' ,'Car' ,'Train' ,]
+print("I would want an " + Transportation[0])
+print("I would want a " + Transportation[1])
+print("I would want a " + Transportation[2])
---
+++ b_modify_lists.py
@@ -0,0 +1,75 @@
+# Q1
+# Guests. If you could invite anyone, living or dead, to dinner, who would you invite?
+# Make a list that includes at least three people you'd like to invite to dinner. Then 
+# use your list to print a message to each person, inviting them to dinner.
+Guests=['Kyle Lowry' ,'Masai Ujiri' ,'Fred Vanvleet']
+Message = "! How are you?, Would you like to join me for dinner?"
+print ("Hey " + (Guests[0]) + Message )
+print ("Hey " + (Guests[1]) + Message)
+print ("Hey " + (Guests[2]) + Message)
+# Q2
+# You just heard that one of the guests can't make the dinner, so you need to send out 
+# a new set of invitiations. You'll have to think of someone else to invite
+Guests.remove('Masai Ujiri')
+Guests.append('Norman Powell')
+Message = "! How are you? Join me for dinner."
+print ( "\n\n Question 2")
+print ("Hey " + (Guests[0]) + Message )
+print ("Hey " + (Guests[1]) + Message)
+print ("Hey " + (Guests[2]) + Message)
+
+
+# - Start with your program from Q1. Add a print() call at the end of your program 
+# stating the name of the guest who can't make it.
+# - Modify your list, replacing the name of the guest who can't make it with the name 
+# of the new person you are inviting.
+# - Print a second send of invitation messages, one for each person who is still in your list.
+
+
+# Q3
+# You just found a bigger dinner table, so now more space is available. Think of three more 
+# guests to invite to dinner.
+#
+# - Start with your program from Q1 and Q2. Add a print() call to the end of your program 
+# informing people that you found a bigger dinner table
+# - Use insert() to add one new guest to the beginning of your list.
+# - Use insert() to add one new guest to the middle of your list.
+# - Use append() to add one new guest to the end of your list.
+# - Print a new set of inivitation messages, one for each person in your list.
+print ( "\n\n Question 3")
+Message = "! Let's party tonight."
+Guests.append('Chris Boucher')
+Guests.insert(0, 'Matt Thomas')
+Guests.insert(3,'Aron Baynes')
+print ("Hey " + (Guests[0]) + Message )
+print ("Hey " + (Guests[1]) + Message )
+print ("Hey " + (Guests[2]) + Message )
+print ("Hey " + (Guests[3]) + Message )
+print ("Hey " + (Guests[4]) + Message ) 
+print ("Hey " + (Guests[5]) + Message ) 
+
+# Q4
+# Shrinking the guest list. You just found out that your new dinner table won't arrive in 
+# time for the dinner, and you have space for only two guests.
+#
+# - Start with your program from Q3. Add a new line that prints a message saying that you 
+# can invite only two people for the dinner.
+# - Use pop() to remove guests from your list one at a time until only two names remain in 
+# your list. Each time you pop a name from your list, print a message to that person letting 
+# them know you are sorry that you can't invite them to dinner
+# - Print a message to each of the two people still on your list, letting them know they 
+# are still invited.
+# - Use del to remove the last two names from your list, so you have an empty list. Print 
+# your list to make sure you actually have an empty list at the end of your prog
+
+Message = ' Sorry the new table did not arrive in time'
+print ("Hey " + (Guests[2]) + Message )
+print ("Hey " + (Guests[3]) + Message )
+print ("Hey " + (Guests[4]) + Message )
+print ("Hey " + (Guests[5]) + Message )
+Guests.sort
+Message = ' You are still invited'
+print ("Hey " + (Guests[0]) + Message )
+print ("Hey " + (Guests[1]) + Message )
+
+
